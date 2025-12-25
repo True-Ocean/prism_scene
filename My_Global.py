17a193d365ca2c758e97bcb570b74b9ce8f432bf
trueocean <trueocean.pikara@gmail.com>
2025-12-26 03:29:21 +0900

    グローバル変数の設定を更新し、レース日、競技場、レース番号、レース名、距離、ハロン数、ファイル名を変更

--- My_Global.py
+++ My_Global.py
@@ -1,19 +1,19 @@
 # グローバル関数
 # g変数の仮設定
 
-race_date = '2025-11-30'
-stadium = '東京'
-r_num = '12R'
-race_name = 'ジャパンカップ'
+race_date = '2025-12-28'
+stadium = '中山'
+r_num = '11R'
+race_name = '有馬記念'
 td = '芝'
-distance = 2400
+distance = 2500
 age = '３上'
 clas = 'Ｇ１'
 cond = '良'
-hr_num = 18
+hr_num = 16
 exe_opt = 0
 scr_opt = True
-filename = '2025-11-30東京12R'
+filename = '2025-12-28中山11R'
 
 mode = 'single'
 
