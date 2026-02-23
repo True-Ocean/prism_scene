cbca4750d7c25ba781107fab20923bf6815217ed
True-Ocean <trueocean.pikara@gmail.com>
2026-02-23 11:18:15 +0900

    パスコードの注釈を微修正

--- App.py
+++ App.py
@@ -151,7 +151,7 @@ def show_new_auth():
             else:
                 st.error("パスコードが違います。")
         
-        st.write('note の記事内にパスコードを無料公開しています。noteでシャノワールをフォローしていただければ嬉しいです。')
+        st.write('note の記事内にパスコードを無料公開しています。')
         st.write('note のリンク先はこちら 👉 https://note.com/prism_scene')
 
 def show_race_content(file_name):
